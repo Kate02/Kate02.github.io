c7db41885f6ab5892ffaba625a76987983deceab
Kate02 <k.kurak@i.ua>
2021-03-25 16:12:48 +0200

    add about

---
+++ precache-manifest.72febd3d1f9c4fbe99a6af48bbf83992.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/1.ico/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/1.ico/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/1.ico/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/1.ico/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/1.ico/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/1.ico/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/1.ico/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/1.ico/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/1.ico/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/1.ico/apple-touch-icon.png"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "/1.ico/browserconfig.xml"
+  },
+  {
+    "revision": "b58fcfa7628c9205cb11a1b2c3e8f99a",
+    "url": "/1.ico/manifest.json"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/1.ico/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/1.ico/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/1.ico/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "a8c05acb4114a057bb8b",
+    "url": "/css/app.6cf0ef95.css"
+  },
+  {
+    "revision": "6a8837d050c2d47b432a",
+    "url": "/css/chunk-vendors.41aa41c3.css"
+  },
+  {
+    "revision": "c00984d4c5b9d26a01aa2d6b7cf14f1c",
+    "url": "/img/Union.c00984d4.svg"
+  },
+  {
+    "revision": "e95b1282718b3aca9d27ff6724f390f0",
+    "url": "/img/i.e95b1282.jpg"
+  },
+  {
+    "revision": "da749b9f7118089807844c0e75c6393f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8c05acb4114a057bb8b",
+    "url": "/js/app.7e1d34b5.js"
+  },
+  {
+    "revision": "6a8837d050c2d47b432a",
+    "url": "/js/chunk-vendors.9de05416.js"
+  },
+  {
+    "revision": "5f513f2d6aa61bf62b3719ea21a3c746",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
